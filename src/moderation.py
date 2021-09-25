df37e4facae7e40410d9ee643e95b7f16a09a91d
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 11:09:29 +1000

    Updates Clear Commands

--- src/moderation.py
+++ src/moderation.py
@@ -6,13 +6,13 @@ class Moderation(commands.Cog):
         self.client = client
 
     @commands.command(aliases=['purge'])
-    @commands.has_permissions(manage_messages=true)
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount = 5):
         await ctx.channel.purge(limit=amount+1)
 
 
-    @command.command(aliases=['purge.all', 'clear.all'])
-    @commands.has_permissions(manage_messages=true)
+    @commands.command(aliases=['purge.all', 'clear.all'])
+    @commands.has_permissions(manage_messages=True)
     async def clearall(self, ctx):
         await ctx.channel.purge()
 

f45c758d0ee931457864bc715821e8f029418734
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 11:34:53 +1000

    Added kick and ban cmd

--- src/moderation.py
+++ src/moderation.py
@@ -16,5 +16,17 @@ class Moderation(commands.Cog):
     async def clearall(self, ctx):
         await ctx.channel.purge()
 
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member :discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f'{member} was just kicked for "{reason}"!')
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def bad(self, ctx, member :discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f'{member} was just banned for "{reason}"!')
+
 def setup(client):
     client.add_cog(Moderation(client))

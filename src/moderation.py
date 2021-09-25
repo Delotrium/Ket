2e9ce10c94248508796d137f904b1de29a6aebc4
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 11:08:20 +1000

    added clear methods

--- src/moderation.py
+++ src/moderation.py
@@ -5,7 +5,16 @@ class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command(aliases=['purge'])
+    @commands.has_permissions(manage_messages=true)
+    async def clear(self, ctx, amount = 5):
+        await ctx.channel.purge(limit=amount+1)
 
+
+    @command.command(aliases=['purge.all', 'clear.all'])
+    @commands.has_permissions(manage_messages=true)
+    async def clearall(self, ctx):
+        await ctx.channel.purge()
 
 def setup(client):
     client.add_cog(Moderation(client))

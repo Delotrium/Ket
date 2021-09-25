8b106bc338aa6c8524a896b7364f3421363e4699
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 15:22:35 +1000

    Dm and VC invite added

--- src/tools.py
+++ src/tools.py
@@ -5,7 +5,20 @@ class Tools(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command(aliases=["pm"])
+    async def dm(self, ctx, member : discord.Member, *, message):
+        await member.send(message + f"\n ``From {ctx.author}``")
+        Author = ctx.author
+        await Author.send("\""+ message + f"\" was sent to {member}!")
 
+
+
+    @commands.command(aliases=['joinvc', 'vc'])
+    async def invitevc(self, ctx, member : discord.Member, *, topic):
+        inv = await ctx.author.voice.channel.create_invite()
+        await member.send(f"``{ctx.author}`` ***has asked you to join the voice chat!***\n Invite: {inv}\nMessage from ``{ctx.author}`` is:\n*``{topic}``*")
+        Author = ctx.author
+        await Author.send(f"Invite was sent to {member}!")
 
 def setup(client):
     client.add_cog(Tools(client))

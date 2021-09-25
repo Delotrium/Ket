0a950d606bcc280ac512498c925076f793a6fc75
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 13:25:16 +1000

    Added Urban Dictionary Search

--- src/beautifulsoup.py
+++ src/beautifulsoup.py
@@ -1,9 +1,23 @@
 import discord
 from discord.ext import commands
+from bs4 import BeautifulSoup
+import requests
 
 class Soup(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    @commands.command(aliases=["define.urban", 'urban', 'search.urban', 'uban.dictionary'])
+    async def define_urban(self, ctx, *, query):
+        url = f"https://www.urbandictionary.com/define.php?term={query}"
+        
+        result=requests.get(url)
+        doc = BeautifulSoup(result.text, "html.parser")
+        try:
+            article = doc.find('div', class_="meaning").text
+            await ctx.send(f"{ctx.author.mention}, The definition of {query} on the urban dictionary is:\n{article}")
+        except:
+            await ctx.send(f"{ctx.author.mention}:\n We could not find \"{query}\" on the urban dictionary!")
 
 
 

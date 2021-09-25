2f4aca3946330749bd775fb1bf12e7981738382e
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 11:01:47 +1000

    Added Shoot command

--- src/fun.py
+++ src/fun.py
@@ -29,9 +29,22 @@ class Fun(commands.Cog):
                    'My sources say no', 
                    'Outlook not so good', 
                    'Very doubtful']
-        await ctx.send(f"{ctx.author.mention} asked: {question}\n My reply is: {answers[random.randint(1, len(answers)-1)]}")
+        await ctx.send(f"{ctx.author.mention} asked: {question}\n My reply is: {answers[random.randint(0, len(answers)-1)]}")
 
 
+    @commands.command(aliases=['kill', 'hunt'])
+    async def shoot(self, ctx, member : discord.Member):
+        replies = [f'Damn nice shot {ctx.author.mention}. You shot {member.mention} right in the head....',
+                   f'{ctx.author.mention}, did you know that was a water gun? You just got {member.mention} all wet... and not in a good way ;)',
+                   f'Uhhh... well done {ctx.author.mention}. The gun was the wrong way.... You are very lucky {member.mention}',
+                   f'{ctx.author.mention}, your gun was taken by {member.mention} after their quick karate skills, better luck next time.',
+                   f'OMG {ctx.author.mention} JUST HIT A 360, NO-SCOPE, WALL JUMP, EYES CLOSED, PLAYING WITH FEET, BUNNY HOP, SHOT ON {member.mention}... MLG!!!!!',
+                   f'{member.mention} ran away... maybe don\'t be so fat {ctx.author.mention}.',
+                   f'{member.mention} pulled out a bigger gun, you better run {ctx.author.mention}.',
+                   f'{ctx.author.mention} did you just hug {member.mention}? That\'s abit weird...',
+                   f'A police officer was looking at you {ctx.author.mention}... you have to pay {member.mention} money...',
+                   f'{ctx.author.mention} chose kick! It was highly effective! {member.mention} is now half HP!']
+        await ctx.send(replies[random.randint(0, 9)])
 
 def setup(client):
     client.add_cog(Fun(client))

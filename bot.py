981a6f1f993bd7aef0115fda2305b81d57094c1e
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-09-25 12:07:38 +1000

    Added Status Chages with whitelist

--- bot.py
+++ bot.py
@@ -2,14 +2,25 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix='tb!')
+client = commands.Bot(command_prefix='ket!')
+
+#Change Status:
+@client.command()
+async def status(ctx, *, stat):
+    whitelist = [433159491404693514, 708113653940879502]
+    if ctx.author.id in whitelist:
+        await client.change_presence(status=discord.Status.online, activity=discord.Game(stat))
+    else:
+        await ctx.send(f"Sorry {ctx.author.mention}, but you have contact the owner of the bot (delotrium) to change my status!")
+
+
 
 @client.command()
 async def load(ctx, extension):
     client.load_extension(f"src.{extension}")
 
 @client.command()
-async def unload(ctx, extension):
+async def   unload(ctx, extension):
     client.unload_extension(f"src.{extension}")
 
 for filename in os.listdir('DiscordBot\src'):
@@ -17,4 +28,5 @@ for filename in os.listdir('DiscordBot\src'):
         client.load_extension(f"src.{filename[:-3]}")
 
 
-client.run("<token>")
+
+client.run("")
